f01d22852e2bf75437ecf16d4d4abff2aa31c18f
Visar <visarmullaj07@gmail.com>
2026-02-22 19:01:29 +0100

    feat: create the categories object

---
+++ constants/categories.ts
@@ -0,0 +1,23 @@
+export const CATEGORIES = {
+  Biologji: ["Ciklet", "Diversiteti", "Ndërveprimet", "Sistemet"],
+  Fizikë: [
+    "Lëkundjet dhe valët, Optika valore",
+    "Magnetizmi",
+    "Optika gjeometrike",
+    "Puna dhe energjia",
+    "Rryma elektrike",
+    "Termodinamika",
+    "Dinamika",
+    "Elektrostatika",
+    "Kinematika",
+    "Fizika kuantike dhe bërthamore",
+  ],
+  Kimi: [
+    "Reaksionet redoks dhe elektrokimia",
+    "Struktura e atomit dhe sistemi periodik",
+    "Termokimia dhe kinetika",
+    "Ekuilibri kimik, acidet-bazat, ekuilibrat jonik",
+    "Kimi organike",
+    "Lidhjet kimike, simbolet, formulat dhe njehsimet stekiometrike",
+  ],
+};
